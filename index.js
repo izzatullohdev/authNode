2b0323d804ddd24b435e3224bf42169b66cf607f
Izzatbek <izzatbekmadaminov7141@gmail.com>
2024-12-26 17:02:49 +0500

    agar "/" bo'lsa "/api-docs" ga o'tkazib yubordik

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ app.use(cors());
 const { swaggerUI, swaggerSpec } = require("./swaggerConfig");
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 // Routes
+
 app.use("/api/v1/auth", require("./routes/auth.route"));
+
+app.use("/", (req, res) => {
+  res.redirect("/api-docs");
+});
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`.bgBlue);
 });
